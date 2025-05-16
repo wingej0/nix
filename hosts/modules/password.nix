baadde90f5b46a1873eb4fecffe9430083a92f26
Jeff Winget <jeff@3dgradebook.com>
2025-05-16 09:28:32 -0600

    updated user module

--- hosts/modules/password.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, lib, ... }:
-{
-    options = with lib; with types; {
-        hashedPassword = mkOption { type = str; };
-    }
-    config = {
-        hashedPassword = "$6$DC6usdc/o.Svf2X3$yyl4T3lbOjCUVma/io5nEWjaUxbl5ly//R39sr6tBHpLQQORaOVluRWfqOwfwSzBSA1/cwJANsEcsDAr1bDIn1";
-    }
-}
