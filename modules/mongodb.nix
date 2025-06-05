5bdcee5bdff7fb15818f97dac0816044884dbf67
Jeff Winget <jeff@3dgradebook.com>
2025-06-05 08:53:16 -0600

    Still working on holysheet server

--- modules/mongodb.nix
+++ modules/mongodb.nix
@@ -8,8 +8,8 @@
   # Enable mongodb
   services.mongodb = {
     enable = true;
-    # enableAuth = true;
-    # initialRootPasswordFile = "/persist/mongodb";
+    enableAuth = true;
+    initialRootPasswordFile = "/home/wingej0/.mongodb_root";
     bind_ip = "0.0.0.0";
   };
 }
