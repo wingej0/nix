81ef850da176682a85d9aad7000670e2198af016
Jeff Winget <jeff@3dgradebook.com>
2025-06-05 11:30:47 -0600

    Adding RDP access to holysheet server

--- modules/remote-access.nix
+++ modules/remote-access.nix
@@ -17,15 +17,8 @@
     };
 
     services.xrdp.enable = true;
-    services.xrdp.defaultWindowManager = "${pkgs.gnome-session}/bin/gnome-session";
+    services.xrdp.defaultWindowManager = "gnome-remote-desktop";
     services.xrdp.openFirewall = true;
-
-    # Disable the GNOME3/GDM auto-suspend feature that cannot be disabled in GUI!
-    # If no user is logged in, the machine will power down after 20 minutes.
-    systemd.targets.sleep.enable = false;
-    systemd.targets.suspend.enable = false;
-    systemd.targets.hibernate.enable = false;
-    systemd.targets.hybrid-sleep.enable = false;
 
     # Open ports in the firewall.
     networking.firewall.allowedTCPPorts = [ 22 3389 27017 ];
