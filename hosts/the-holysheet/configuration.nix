afd10c8b0f5d0cd045b1f010b2c867dbccc785f7
Jeff Winget <jeff@3dgradebook.com>
2025-06-09 07:41:33 -0600

    Switch to Cinnamon for holysheet server for better remote access.

--- hosts/the-holysheet/configuration.nix
+++ hosts/the-holysheet/configuration.nix
@@ -45,9 +45,10 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
+  # Enable Cinnamon
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.desktopManager.cinnamon.enable = true;
+
 
   # Configure keymap in X11
   services.xserver.xkb = {
