f5d3071df89235008a581866cae9ff8519dac8ee
Jeff Winget <jeff@3dgradebook.com>
2025-06-23 07:33:09 -0600

    Removed specialisations to just run Cosmic

--- desktops/cinnamon.nix
+++ desktops/cinnamon.nix
@@ -1,16 +1,14 @@
 { config, lib, pkgs, ... }:
 {
-    specialisation = {
-        cinnamon-desktop.configuration = {
-            # Enable the X11 windowing system.
-            services.xserver.enable = true;
+    
+    # Enable the X11 windowing system.
+    services.xserver.enable = true;
 
-            # Enable Cinnamon
-            services.xserver.displayManager.lightdm.enable = true;
-            services.xserver.desktopManager.cinnamon.enable = true;
+    # Enable Cinnamon
+    services.xserver.displayManager.lightdm.enable = true;
+    services.xserver.desktopManager.cinnamon.enable = true;
 
-            # Enable flatpaks
-            services.flatpak.enable = true;
-        };
-    };
+    # Enable flatpaks
+    services.flatpak.enable = true;
+        
 }
--- desktops/xfce.nix
+++ desktops/xfce.nix
@@ -1,99 +1,94 @@
 { config, lib, pkgs, inputs, username, ... }:
 {
     imports = [
-        # (_: { nixpkgs.overlays = [ inputs.qtile-flake.overlays.default ]; })
-        # ./../overlays/qtile-overlay.nix
+        (_: { nixpkgs.overlays = [ inputs.qtile-flake.overlays.default ]; })
+        ./../overlays/qtile-overlay.nix
     ];
 
-    specialisation = {
-        xfce-desktop.configuration = {
-
-            services.xserver = {
-                enable = true;   
-                desktopManager = {
-                    xterm.enable = false;
-                xfce = {
-                    enable = true;
-                };
-            };
-
-            windowManager.qtile = {
+    services.xserver = {
+        enable = true;   
+        desktopManager = {
+            xterm.enable = false;
+            xfce = {
                 enable = true;
-                extraPackages = python3Packages: with python3Packages; [
-                    qtile-extras
-                ];
             };
-
         };
-        services.displayManager.defaultSession = "xfce";
-        services.gnome.gnome-keyring.enable = true;
 
-        # Enable flatpaks
-        services.flatpak.enable = true;
-        services.blueman.enable = true;
-
-        xdg.portal = {
+        windowManager.qtile = {
             enable = true;
-            config.common.default = "*";
-            extraPortals = with pkgs; [
-                xdg-desktop-portal-gtk
+            extraPackages = python3Packages: with python3Packages; [
+                qtile-extras
             ];
         };
 
-        environment.systemPackages = with pkgs; [
-
-                # X11 Programs
-                picom
-                flameshot
-                peek
-                variety
-                wallust
-                rofi
-                brightnessctl
-                python3
-                haskellPackages.greenclip
-                file-roller
-                playerctl
-            ];
-
-        home-manager.users.${username} = {
-
-            programs.zsh.initContent = ''
-                cat ~/.cache/wallust/sequences
-            '';
-
-            gtk = {
-                enable = true;
+    };
     
-                gtk3 = {
-                    extraConfig = {
-                        gtk-application-prefer-dark-theme = 1;
-                    };
-                };
+    services.displayManager.defaultSession = "xfce";
+    services.gnome.gnome-keyring.enable = true;
 
-                gtk4 = {
-                    extraConfig = {
-                        gtk-application-prefer-dark-theme = 1;
-                    };
-                };
+    # Enable flatpaks
+    services.flatpak.enable = true;
+    services.blueman.enable = true;
 
-                font = {
-                    name = "Fira Code Nerd Font";
-                    size = 11;
-                };
+    xdg.portal = {
+        enable = true;
+        config.common.default = "*";
+        extraPortals = with pkgs; [
+            xdg-desktop-portal-gtk
+        ];
+    };
 
-                theme = {
-                    name = "Orchis";
-                    package = pkgs.orchis-theme;
-                };
+    environment.systemPackages = with pkgs; [
 
-                iconTheme = {
-                    name = "Tela-dark";
-                    package = pkgs.tela-icon-theme;
+            # X11 Programs
+            picom
+            flameshot
+            peek
+            variety
+            wallust
+            rofi
+            brightnessctl
+            python3
+            haskellPackages.greenclip
+            file-roller
+            playerctl
+        ];
+
+    home-manager.users.${username} = {
+
+        programs.zsh.initContent = ''
+            cat ~/.cache/wallust/sequences
+        '';
+
+        gtk = {
+            enable = true;
+
+            gtk3 = {
+                extraConfig = {
+                    gtk-application-prefer-dark-theme = 1;
                 };
             };
+
+            gtk4 = {
+                extraConfig = {
+                    gtk-application-prefer-dark-theme = 1;
+                };
+            };
+
+            font = {
+                name = "Fira Code Nerd Font";
+                size = 11;
+            };
+
+            theme = {
+                name = "Orchis";
+                package = pkgs.orchis-theme;
+            };
+
+            iconTheme = {
+                name = "Tela-dark";
+                package = pkgs.tela-icon-theme;
+            };
         };
-    };
- 
     };
 }
