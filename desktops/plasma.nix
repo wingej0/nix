e15096554042acc28d560f4e997ceed7cda2f8d3
Jeff Winget <jeff@3dgradebook.com>
2025-11-25 09:48:03 -0700

    Added gnome-keyring to plasma.nix

--- desktops/plasma.nix
+++ desktops/plasma.nix
@@ -4,4 +4,14 @@
     services.displayManager.sddm.wayland.enable = true;
     services.desktopManager.plasma6.enable = true;
 
+    # Enable gnome-keyring for applications like Mailspring and MongoDB Compass
+    services.gnome.gnome-keyring.enable = true;
+    security.pam.services.sddm.enableGnomeKeyring = true;
+
+    # Ensure keyring packages are available
+    environment.systemPackages = with pkgs; [
+        gnome-keyring
+        libsecret
+    ];
+
 }
