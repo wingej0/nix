cdb30b604e3e099c5ba9e30ab8f3bb9831df784c
Jeff Winget <jeff@3dgradebook.com>
2026-02-16 15:20:55 -0700

    Fixed login issues in qtile-wayland by swapping out sddm for tuigreet

--- hosts/darter-pro/configuration.nix
+++ hosts/darter-pro/configuration.nix
@@ -19,8 +19,7 @@
 
   # Hibernation configuration
   boot.resumeDevice = "/dev/disk/by-uuid/d37128be-6d28-497f-a491-f2d40f9b2372";
-  boot.kernelParams = [ "resume=/dev/disk/by-uuid/d37128be-6d28-497f-a491-f2d40f9b2372" "quiet" "loglevel=3" ];
-  boot.consoleLogLevel = 0;
+  boot.kernelParams = [ "resume=/dev/disk/by-uuid/d37128be-6d28-497f-a491-f2d40f9b2372" ];
 
   # Networking
   networking.hostName = "darter-pro";
